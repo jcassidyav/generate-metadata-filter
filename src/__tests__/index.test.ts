a1dc398fee4a86123279f493bcb4373c0c3be05e
AVAEON\jcassidy <jcassidy@avaeon.com>
2022-06-24 16:39:07 +0100

    test: remove useless tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,5 @@
 import test from "ava";
 import { ConfigX } from "../config";
-import { foo, bar } from "../index";
-
-test("foo()", (t) => {
-    t.is(foo(1, 2), 5);
-});
-
-test("bar()", (t) => {
-    t.is(bar(2, 1), 1);
-});
 
 test("config", (t) => {
     t.is(ConfigX.readConfig().typeSources.android[0], "a");
